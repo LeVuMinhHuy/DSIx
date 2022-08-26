0c0e2762fa923176e724edbd678bc08c831f5d3a
Ted Vu <ted.vu@sourcecode.com.au>
2022-08-26 21:47:50 +0700

    [26.08.22] len == cap -> realloc

--- vec/src/lib.rs
+++ vec/src/lib.rs
@@ -1,18 +1,32 @@
-use std::alloc::{alloc, Layout};
+use std::alloc::{alloc, dealloc, realloc, Layout};
 use std::ptr::NonNull;
 
 /// why we need **ptr** in struct vector is a *raw vector* - NonNull ?
 
 #[allow(dead_code)]
-struct MyVec<T> {
+pub struct MyVec<T> {
     ptr: NonNull<T>,
     capacity: usize,
     len: usize,
 }
 
+// TODO: need to check again this drop trait to understand
+impl<T> Drop for MyVec<T> {
+    fn drop(&mut self) {
+        unsafe {
+            std::ptr::drop_in_place(std::slice::from_raw_parts_mut(self.ptr.as_ptr(), self.len));
+            let size = std::mem::size_of::<T>() * self.capacity;
+            let align = std::mem::align_of::<T>();
+
+            let layout = Layout::from_size_align(size, align).expect("Cannot get layout of T");
+            dealloc(self.ptr.as_ptr() as *mut u8, layout)
+        }
+    }
+}
+
 #[allow(dead_code)]
 impl<T> MyVec<T> {
-    fn new() -> Self {
+    pub fn new() -> Self {
         Self {
             ptr: NonNull::dangling(),
             capacity: 0,
@@ -20,14 +34,13 @@ impl<T> MyVec<T> {
         }
     }
 
-    unsafe fn push(&mut self, item: T) {
+    pub fn push(&mut self, item: T) {
         if self.capacity == 0 {
             // init a block of data with size: 4 * size_of::<T>
             let new_space_in_heap = Layout::array::<T>(4).expect("Cannot create layout");
 
             // allocate a layout of data onto heap
-            let ptr: *mut T = alloc(new_space_in_heap) as *mut T;
-
+            let ptr: *mut T = unsafe { alloc(new_space_in_heap) as *mut T };
             // init new NonNull raw pointer with the pointer to new space data in heap
             // store to myvec.ptr
             self.ptr = NonNull::new(ptr).expect("NonNull::new error");
@@ -35,26 +48,67 @@ impl<T> MyVec<T> {
             // now we have enough space for store item, let's store it
             // we convert myvec ptr back to raw pointer and write item to the heap
             // note: why we don't do this: `*self.ptr.as_ptr() = item` ?
-            self.ptr.as_ptr().write(item);
-
+            unsafe {
+                self.ptr.as_ptr().write(item);
+            }
             // update capacity of myvec to 4
             self.capacity = 4;
 
             // update len of myvec to 1
             self.len = 1
         } else if self.len < self.capacity {
-            self.ptr.as_ptr().add(self.len).write(item)
+            unsafe { self.ptr.as_ptr().add(self.len).write(item) }
+
+            // increase len by 1
+            self.len += 1;
         } else {
-            // TODO: realloc
-            todo!()
+            // get block of memory from size (all vector) and alignment of T
+            let size = std::mem::size_of::<T>() * self.capacity;
+            let align = std::mem::align_of::<T>();
+            size.checked_add(size % align).expect("Cannot alloc!");
+
+            let layout = Layout::from_size_align(size, align).expect("Cannot get layout of T");
+
+            // new size = current capacity * 2
+            let new_capacity = self
+                .capacity
+                .checked_mul(2)
+                .expect("Muliple overflow capacity");
+
+            println!(
+                "size: {}, align: {}, new_capacity: {}",
+                size, align, new_capacity
+            );
+
+            let ptr = unsafe {
+                println!("{:?}", layout);
+
+                let realloc_ptr: *mut T =
+                    realloc(self.ptr.as_ptr() as *mut u8, layout, new_capacity) as *mut T;
+
+                let ptr = NonNull::new(realloc_ptr).expect("NonNull::new error");
+
+                // Store new item to reallocated heap
+                ptr.as_ptr().write(item);
+
+                ptr
+            };
+
+            self.ptr = ptr;
+
+            // increase len by 1
+            self.len += 1;
+
+            // assign new capacity
+            self.capacity = new_capacity;
         }
     }
 
-    fn capacity(&self) -> usize {
+    pub fn capacity(&self) -> usize {
         self.capacity
     }
 
-    fn len(&self) -> usize {
+    pub fn len(&self) -> usize {
         self.len
     }
 }
@@ -63,27 +117,43 @@ impl<T> MyVec<T> {
 mod tests {
     use super::*;
 
-    #[test]
-    fn std_vec() {
-        let mut vec_sample = Vec::<u32>::new();
-        vec_sample.push(1);
-        vec_sample.push(2);
-        vec_sample.push(3);
-        vec_sample.push(4);
-        vec_sample.push(5);
+    //#[test]
+    //fn std_vec() {
+    //    let mut vec_sample = Vec::<u32>::new();
+    //    vec_sample.push(1);
+    //    vec_sample.push(2);
+    //    vec_sample.push(3);
+    //    vec_sample.push(4);
+    //    vec_sample.push(5);
 
-        //println!("{}", vec_sample.capacity());
+    //    //println!("{}", vec_sample.capacity());
 
-        assert_eq!(vec_sample, [1, 2, 3, 4, 5]);
-        assert_eq!(vec_sample.capacity(), 8);
-        assert_eq!(vec_sample.len(), 5)
-    }
+    //    assert_eq!(vec_sample, [1, 2, 3, 4, 5]);
+    //    assert_eq!(vec_sample.capacity(), 8);
+    //    assert_eq!(vec_sample.len(), 5)
+    //}
 
     #[test]
     fn my_vec() {
-        let my_vec_sample = MyVec::<u32>::new();
+        let mut my_vec_sample = MyVec::<u32>::new();
 
-        assert_eq!(my_vec_sample.capacity(), 0);
-        assert_eq!(my_vec_sample.len(), 0)
+        my_vec_sample.push(1);
+        my_vec_sample.push(2);
+        my_vec_sample.push(3);
+        my_vec_sample.push(4);
+        my_vec_sample.push(5);
+        my_vec_sample.push(6);
+        my_vec_sample.push(7);
+
+        // ======================
+        // This causes error
+        // my_vec_sample.push(8);
+        // my_vec_sample.push(9);
+
+        //TODO: [Error] realloc(): invalid next size
+        // when realloc more than 2
+
+        assert_eq!(my_vec_sample.capacity(), 8);
+        assert_eq!(my_vec_sample.len(), 7)
     }
 }
