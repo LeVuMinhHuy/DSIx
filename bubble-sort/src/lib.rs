55db262ff9410fb0b48854cdba56e0e455bca5d6
Ted Vu <ted.vu@sourcecode.com.au>
2022-10-03 09:28:33 +0700

    Init bubble sort

--- bubble-sort/src/lib.rs
+++ bubble-sort/src/lib.rs
@@ -1,5 +1,17 @@
-//impl<T> BubbleSort for [T] {
-//    fn bubble_sort(&mut self) -> &Self {
-//        println!("{}", self)
-//    }
-//}
+pub fn bubble_sort<const N: usize>(array: [u32; N]) {
+    for x in array {
+        print!("{x}, ")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn normal_array() {
+        let array: [u32; 8] = [2, 3, 1, 4, 9, 6, 2, 1];
+
+        super::bubble_sort(array);
+
+        assert_eq!(array, [1, 1, 2, 2, 3, 4, 6, 9])
+    }
+}
